1cef22b7fed175464a7e18a9619df565be815cd3
Islom <example@gmail.com>
2023-06-09 11:53:18 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,156 @@
+"use strict";
+
+// Ваш код в строгом режиме
+ 
+// a = 15;
+// console.log(a)
+
+
+
+
+
+// let number = 5;
+// const ledtBorderWidth = 10;
+
+// number = 10;
+// console.log(number);
+
+
+
+
+
+// let a = 1200,
+//     b =2000;
+
+
+//     console.log("Lol i have:" + a);
+
+
+//     ['vruvru', 'lol'].map(a => ); 
+
+
+//snake_case
+
+
+// Класфикация типов данных в JS
+
+
+// const obj = {
+//     Name: 'Islom',
+//     age: 21,
+//     ismArried: false
+// };
+
+// console.log(obj.age);
+
+
+
+
+
+// const arr = [1, 2, "string"];
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     abc: {
+//         dff: [{}, {}],
+//         def:{
+
+//         }
+//     }
+// }  // Также можно записать так, как пример с фильмов где фильмф рахделыютмя по категориям
+
+
+// const b ='b'
+
+
+
+// arrObj[b] = 'Bro' // Из-за того что тут нету такого общекта он сощлает новый 
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+
+
+// let storeName = "VipBrand",
+//     storeDescription = {
+//         budget: 10000,
+//         employees: ['Ivan', "Nikolay", 'Petr'],
+//         products: {
+            // ris: 11000,
+    //         cola: 10000
+    //     },
+    //     open: true
+    // };
+
+    // let store_Name = "Exxos",
+    // store_Description = {
+    //     budget: 10000,
+    //     employees: ['Islom', "Rustam", 'Abdulla'],
+    //     products: {
+    //         meat: 1200,
+    //         water: 6200,
+    //         chicken: 12000
+    //     },
+    //     open: true
+    // };
+
+
+
+    // Work with users
+
+
+// const result = +prompt("What do yoy have?", '');
+// // console.log(typeof(result));
+// console.log(result + 10);
+
+
+
+// const answers = [];
+
+// answers[0]  = prompt('First Name', '');
+// answers[1]  = prompt('Last Name', '');
+// answers[2]  = prompt('Age', '');
+
+
+// document.write(answers);
+
+
+
+
+// Интерполяция
+
+
+// const category = "toys";
+
+//  console.log(`https://lol.com${category}/5`);
+
+
+
+
+
+
+// Операторы  JS
+
+
+console.log('Islom' + ' - TopG');
+console.log(4 +   +'10'); 
+
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+ 
+
+    console.log(incr);
+    console.log(decr);
+
+    const lol = true,
+          bro = false;
+
+
+          console.log(lol || !bro);
+           
